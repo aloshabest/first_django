624396c7808be6b48138a3eeac9e37854dd5881e
Алексей <stormwww@yandex.ru>
2022-05-27 16:42:50 +0300

    models db

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField
+    created_ed = models.DateTimeField(auto_now_add=True)
+    updated_ed = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    is_published = models.BooleanField(default=True)
