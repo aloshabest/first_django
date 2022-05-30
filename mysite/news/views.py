42906bbaec2daf982ce35455f318fb13e569edfc
Алексей <stormwww@yandex.ru>
2022-05-30 12:17:05 +0300

    templates

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import News
 
 
 def index(request):
-    return HttpResponse('Hello World')
+    news = News.objects.all()
+    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
 
 
-def test(request):
-    return HttpResponse('<h1>Test list</h1>')
