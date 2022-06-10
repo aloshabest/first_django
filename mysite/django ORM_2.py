adbb62a008b8bf2a4b50dc8d6cd24b3933e54770
Алексей <stormwww@yandex.ru>
2022-06-10 16:41:09 +0300

    filters

--- mysite/django ORM_2.py
+++ mysite/django ORM_2.py
@@ -8,4 +8,21 @@
 # <QuerySet [<News: Form news 2>]>
 # >>> News.objects.filter(pk__in=[1, 2])
 # <QuerySet [<News: Новость из админки>, <News: Новость 1>]>
+# >>> News.objects.first
+# <bound method BaseManager._get_queryset_methods.<locals>.create_method.<locals>.manager_method of <django.db.models.manager.Manager object at 0x000001EE6BCCBF70>>
+# >>> News.objects.first()
+# <News: «Аэрофлот» прекратил продажу билетов на Шри-Ланку после ареста самолета>
+# >>> News.objects.order_by('pk').first()
+# <News: Новость 1>
+# >>> News.objects.earliest('created_ed')
+# <News: Новость 1>
+# >>> News.objects.latest('created_ed')
+# <News: «Аэрофлот» прекратил продажу билетов на Шри-Ланку после ареста самолета>
+# >>> news = News.objects.get(pk=3)
+# >>> news
+# <News: Неопубликованная>
+# >>> news.get_previous_by_created_ed()
+# <News: Новость из админки>
+# >>> news.get_next_by_created_eqd()
+# <News: Form>
 
